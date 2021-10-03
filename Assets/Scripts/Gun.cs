769028da923ed4b25d0fa3e0d0a3841c32fadaa5
nickhildebrant <nickhildebrant09@gmail.com>
2021-10-03 14:20:33 -0700

    Baked NavMesh and Animations

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,16 +9,31 @@ public class Gun : MonoBehaviour
     [SerializeField]
     private Camera fpsCam;
 
+    [SerializeField] private Transform slide;
+    [SerializeField] private Transform gun;
+
+    private bool isShooting = false;
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Fire1")){
             Shoot();
         }
+
+        if (slide.localPosition.z > -.01f && isShooting) {
+            slide.localPosition = new Vector3(0f, 0f, slide.localPosition.z - .0005f);
+            //gun.Rotate(new Vector3(-.5f, 0f, 0f));
+        } else if(slide.localPosition.z < -.01f){
+            slide.localPosition = new Vector3(0f, 0f, 0f);
+            //gun.rotation.Set(0f, 0f, 0f, 0f);
+            isShooting = false;
+        }
     }
 
     private void Shoot(){
         RaycastHit hit;
+        isShooting = true;
         if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward, out hit, range)){
             Debug.Log(hit.transform.name);
         }
---
+++ Assets/Scripts/Pathing.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathing : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
